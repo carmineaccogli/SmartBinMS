bd929b997fdbd53ebf5a9df2a096c0b61068051d
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2023-10-02 10:19:14 +0200

    Changed CORS Configuration Policy

--- src/main/java/it/unisalento/pas/smartcitywastemanagement/smartbinms/configuration/SecurityConfig.java
+++ src/main/java/it/unisalento/pas/smartcitywastemanagement/smartbinms/configuration/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
@@ -23,7 +28,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.cors(corsPolicy -> corsPolicy.disable())
+        http.cors(corsPolicy -> corsConfigurationSource())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests.anyRequest().authenticated())
@@ -41,5 +46,16 @@ public class SecurityConfig {
         return new JwtAuthenticationFilter();
 
     }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // to change with API GATEWAY
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 
 }
