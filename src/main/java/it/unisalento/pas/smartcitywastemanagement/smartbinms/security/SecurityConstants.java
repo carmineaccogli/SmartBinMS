7e10cc925dbdb9c69c14476f2b4fc8de58392a8b
carmine.accogli01 <carmine.accogli01@gmail.com>
2024-01-24 18:17:00 +0100

    .gitignore added element

--- src/main/java/it/unisalento/pas/smartcitywastemanagement/smartbinms/security/SecurityConstants.java
+++
@@ -1,7 +0,0 @@
-package it.unisalento.pas.smartcitywastemanagement.smartbinms.security;
-
-public class SecurityConstants {
-    public static final String JWT_SECRET = "seedseedseedseedseedseedseedseedseedseedseed"; // sarà utilizzato per l'algoritmo di firma
-
-    public static final String THIS_MICROSERVICE ="http://smartBinsManagementService:8081";
-}
